cb96754fdb2a50cddb2f473c7f66d5f7f5383499
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-21 07:38:08 -0600

    Completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
         body:
         SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 60,
@@ -21,9 +23,59 @@ class MyApp extends StatelessWidget {
               Text('Jaime Arriola',style: TextStyle(
                 fontSize: 40,
                 color: Colors.white,
-                
+                fontFamily: 'Pacifico',
                 fontWeight: FontWeight.bold
-              ),)
+              ),),
+              Text('Frontend Developer',style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  letterSpacing: 2,
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold
+              ),),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 25),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.computer,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                  title: Text('jaime.arriola.netlify.app',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontFamily: 'SourceSansPro'
+                    ),),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 25),
+
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                    size: 50,
+                  ),
+                  title: Text('arriola.dev@outlook.com',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontFamily: 'SourceSansPro'
+                    ),),
+                ),
+              )
             ],
           ),
         ),
@@ -31,3 +83,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
